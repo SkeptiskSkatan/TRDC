963fd7b4efb3d4da7b9f6f9eb25de7d61dd2b51a
Wicmo <joel.edvardsson05@gmail.com>
2026-05-09 14:47:41 +0200

    started map generation

--- Terminal Roguelike Dungeon Crawler/Terminal Roguelike Dungeon Crawler.cpp
+++ Terminal Roguelike Dungeon Crawler/Terminal Roguelike Dungeon Crawler.cpp
@@ -1,9 +1,31 @@
 // Terminal Roguelike Dungeon Crawler.cpp
 #include <iostream>
 
+enum tile {
+    WALL,
+    FLOOR,
+    DOOR,
+    STAIRS
+};
+
+const int width = 20;
+
+const int height = 40;
+
+tile grid[width][height];
+
+void generate_map(tile grid[width][height]) {
+
+
+
+
+
+
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
 
-    //teststetstetsetstetstestetstetsetstettsettset
 }
